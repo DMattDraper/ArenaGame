94c41361da80ba180400dac258e95c3b9c453627
DDraper7 <david.draper7@t-mobile.com>
2021-04-10 16:13:26 -0400

    Implemented dynamic attack icon

---
+++ Assets/Scripts/UI/Attack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Attack : MonoBehaviour
+{
+    public Image bomb;
+    public Image bow;
+    public Image blade;
+
+    public void activateBomb() {
+        bomb.enabled = true;
+        bow.enabled = false;
+        blade.enabled = false;
+    }
+
+    public void activateBow() {
+        bomb.enabled = false;
+        bow.enabled = true;
+        blade.enabled = false;
+    }
+
+    public void activateBlade() {
+        bomb.enabled = false;
+        bow.enabled = false;
+        blade.enabled = true;
+    }
+}
